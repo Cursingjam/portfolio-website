bb12b8e3f9ca6f824bc9cf3c554d5f0cb07ed0bf
Cursingjam <launte13@gmail.com>
2025-12-04 14:53:00 -0500

    commit here

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,17 +50,17 @@ export const Contact = () => {
             message: "",
         });
 
-        try{
+        try {
 
             await emailjs.send(
-                import.meta.env.VITE_EMAILJS_SERVICE_ID, 
-                import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
-                {
+                import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                    {
                     name: formData.name,
                     email: formData.email,
                     message: formData.message,
-                }
-            );
+                    }
+                );
 
             setFormStatus({
             submitting: false,
